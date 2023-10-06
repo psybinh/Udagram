35935a09b688e852e0368d27b761da807e64fbe9
Phung Binh <psybinh@gmail.com>
2023-10-06 18:20:28 +0700

    edit

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -27,56 +27,58 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+function validateURL(pURL: string) {
+  var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+  var url = new RegExp(regexQuery,"i");
+  return url.test(pURL);
+}
+
+  app.get( "/filteredimage", async ( req, res ) => {
+
+    // 1. validate the image_url query
+
+    var image_url = req.query.image_url;
+    var is_image_url_valid = validateURL(image_url);
+
+    if(is_image_url_valid){
+      // 2. call filterImageFromURL(image_url) to filter the image
+      var image_path = await filterImageFromURL(image_url);
+
+      var options = {
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      };
+      // 3. send the resulting file in the response
+      res.sendFile(image_path, options, function (err) {
+        if (err) {
+          res.status(400).send('Image could not be accessed')
+        } else {
+          // 4. deletes any files on the server on finish of the response
+          deleteLocalFiles([image_path]);
+        }
+      });
+
+    }
+    else {
+      res.status(404).send('URL for the image was not found')
+    }
+  });
+
+
   /**************************************************************************** */
 
-  function validateURL(pURL: string) {
-    var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
-    var url = new RegExp(regexQuery,"i");
-    return url.test(pURL);
-  }
-  
-    app.get( "/filteredimage", async ( req, res ) => {
-  
-      // 1. validate the image_url query
-  
-      var image_url = req.query.image_url;
-      var is_image_url_valid = validateURL(image_url);
-  
-      if(is_image_url_valid){
-        // 2. call filterImageFromURL(image_url) to filter the image
-        var image_path = await filterImageFromURL(image_url);
-  
-        var options = {
-          dotfiles: 'deny',
-          headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-          }
-        };
-        // 3. send the resulting file in the response
-        res.sendFile(image_path, options, function (err) {
-          if (err) {
-            res.status(400).send('Image could not be accessed')
-          } else {
-            // 4. deletes any files on the server on finish of the response
-            deleteLocalFiles([image_path]);
-          }
-        });
-  
-      }
-      else {
-        res.status(404).send('URL for the image was not found')
-      }
-    });
-
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
+
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
